f432c040cddc211a83650223c4d3209318df9b91
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-11 12:24:54 +0600

    Dependency injection

--- advanced/src/main/java/com/example/advanced/customer/CustomerController.java
+++ advanced/src/main/java/com/example/advanced/customer/CustomerController.java
@@ -1,13 +1,22 @@
 package com.example.advanced.customer;
 
 import com.example.advanced.AdvancedApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping("/")
     Customer getCustomer(){
-        return new Customer(1L,"John Doe");
+        return customerService.getCustomer();
     }
 }
--- advanced/src/main/java/com/example/advanced/customer/CustomerService.java
+++ advanced/src/main/java/com/example/advanced/customer/CustomerService.java
@@ -1,4 +1,10 @@
 package com.example.advanced.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
+    Customer getCustomer(){
+        return new Customer(1L,"John Doe");
+    }
 }
