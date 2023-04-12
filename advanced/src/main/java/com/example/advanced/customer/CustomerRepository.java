d3d25c26eeecf5c276698f9e00f9ceeb253fe98b
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-12 10:26:24 +0600

    Switching implementations

--- advanced/src/main/java/com/example/advanced/customer/CustomerRepository.java
+++ advanced/src/main/java/com/example/advanced/customer/CustomerRepository.java
@@ -1,14 +1,20 @@
 package com.example.advanced.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Component
+@Primary
 public class CustomerRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
-        return Collections.emptyList();
+
+        return Collections.singletonList(
+                new Customer(1L,"TODO. Implement real db")
+        );
     }
 }
--- advanced/src/main/java/com/example/advanced/customer/CustomerService.java
+++ advanced/src/main/java/com/example/advanced/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.advanced.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,7 +11,9 @@ public class CustomerService {
 
     private final CustomerRepo customerRepo;
 
-    public CustomerService(CustomerRepo customerRepo) {
+    @Autowired
+    public CustomerService(
+            CustomerRepo customerRepo) {
         this.customerRepo = customerRepo;
     }
 
