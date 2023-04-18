fd02db7520cf422aae8dfea8563468ba78950fa2
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-18 16:35:24 +0600

    OpenFeign version fixed

--- advanced/src/main/java/com/example/advanced/AdvancedApplication.java
+++ advanced/src/main/java/com/example/advanced/AdvancedApplication.java
@@ -2,10 +2,12 @@ package com.example.advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
